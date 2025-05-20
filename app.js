ffeed6efe9c52559bbd216bbc013718004a2cdf0
tusharbharambe2005 <tusharbharambe2005@gmail.com>
2025-05-20 13:09:41 -0400

    add

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ app.use((req,res,next)=>{
 //     res.send(registerUser)
 // })
 
-app.use("/",listingsRouter)
+// app.use("/",listingsRouter)      
 app.use("/listings",listingsRouter)
 app.use("/listings/:id/reviews",reviewsRouter)
 app.use("/",userRouter)
